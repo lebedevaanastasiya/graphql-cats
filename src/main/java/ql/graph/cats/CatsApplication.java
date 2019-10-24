bc4dca63b5cb46db73286457a3c92e8d7e246e3d
lebedeanastasija <lebedeanastasija@yandex.by>
2019-10-24 12:57:05 +0300

    fix auth config

--- src/main/java/ql/graph/cats/CatsApplication.java
+++ src/main/java/ql/graph/cats/CatsApplication.java
@@ -1,12 +1,12 @@
 package ql.graph.cats;
 
-import co.dffrntlab.auth.AuthLibraryConfiguration;
+import co.dffrntlab.auth.DffrntAuthConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(AuthLibraryConfiguration.class)
+@Import(DffrntAuthConfiguration.class)
 public class CatsApplication {
 
     public static void main(String[] args) {
